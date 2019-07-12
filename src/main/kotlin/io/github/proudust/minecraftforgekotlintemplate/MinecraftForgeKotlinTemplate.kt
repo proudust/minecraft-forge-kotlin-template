6b09313d83603fe82c637f52941f96072e03685d
Proudust <proudust@gmail.com>
2019-07-12 12:00:05 +0900

    :construction: convert mod class with Intellij IDEA

--- src/main/kotlin/io/github/proudust/minecraftforgekotlintemplate/MinecraftForgeKotlinTemplate.kt
+++ src/main/kotlin/io/github/proudust/minecraftforgekotlintemplate/MinecraftForgeKotlinTemplate.kt
@@ -1,38 +1,24 @@
-package io.github.proudust.minecraftforgekotlintemplate;
+package io.github.proudust.minecraftforgekotlintemplate
 
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block
+import net.minecraft.item.Item
+import net.minecraftforge.event.RegistryEvent
+import net.minecraftforge.fml.common.event.FMLInitializationEvent
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.registry.GameRegistry
 
-@Mod(
-        modid = MinecraftForgeKotlinTemplate.MOD_ID,
-        name = MinecraftForgeKotlinTemplate.MOD_NAME,
-        version = MinecraftForgeKotlinTemplate.VERSION
-)
-public class MinecraftForgeKotlinTemplate {
-
-    public static final String MOD_ID = "minecraft-forge-kotlin-template";
-    public static final String MOD_NAME = "Minecraft Forge Kotlin Template";
-    public static final String VERSION = "2019.1-1.2.23";
-
-    /**
-     * This is the instance of your mod as created by Forge. It will never be null.
-     */
-    @Mod.Instance(MOD_ID)
-    public static MinecraftForgeKotlinTemplate INSTANCE;
+@Mod(modid = MinecraftForgeKotlinTemplate.MOD_ID, name = MinecraftForgeKotlinTemplate.MOD_NAME, version = MinecraftForgeKotlinTemplate.VERSION)
+class MinecraftForgeKotlinTemplate {
 
     /**
      * This is the first initialization event. Register tile entities here.
      * The registry events below will have fired prior to entry to this method.
      */
     @Mod.EventHandler
-    public void preinit(FMLPreInitializationEvent event) {
+    fun preinit(event: FMLPreInitializationEvent) {
 
     }
 
@@ -40,7 +26,7 @@ public class MinecraftForgeKotlinTemplate {
      * This is the second initialization event. Register custom recipes
      */
     @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
+    fun init(event: FMLInitializationEvent) {
 
     }
 
@@ -48,7 +34,7 @@ public class MinecraftForgeKotlinTemplate {
      * This is the final initialization event. Register actions from other mods here
      */
     @Mod.EventHandler
-    public void postinit(FMLPostInitializationEvent event) {
+    fun postinit(event: FMLPostInitializationEvent) {
 
     }
 
@@ -57,35 +43,31 @@ public class MinecraftForgeKotlinTemplate {
      * based on their registry name.
      */
     @GameRegistry.ObjectHolder(MOD_ID)
-    public static class Blocks {
-      /*
+    class Blocks/*
           public static final MySpecialBlock mySpecialBlock = null; // placeholder for special block below
       */
-    }
 
     /**
      * Forge will automatically look up and bind items to the fields in this class
      * based on their registry name.
      */
     @GameRegistry.ObjectHolder(MOD_ID)
-    public static class Items {
-      /*
+    class Items/*
           public static final ItemBlock mySpecialBlock = null; // itemblock for the block above
           public static final MySpecialItem mySpecialItem = null; // placeholder for special item below
       */
-    }
 
     /**
      * This is a special class that listens to registry events, to allow creation of mod blocks and items at the proper time.
      */
     @Mod.EventBusSubscriber
-    public static class ObjectRegistryHandler {
+    object ObjectRegistryHandler {
         /**
          * Listen for the register event for creating custom items
          */
         @SubscribeEvent
-        public static void addItems(RegistryEvent.Register<Item> event) {
-           /*
+        fun addItems(event: RegistryEvent.Register<Item>) {
+            /*
              event.getRegistry().register(new ItemBlock(Blocks.myBlock).setRegistryName(MOD_ID, "myBlock"));
              event.getRegistry().register(new MySpecialItem().setRegistryName(MOD_ID, "mySpecialItem"));
             */
@@ -95,11 +77,24 @@ public class MinecraftForgeKotlinTemplate {
          * Listen for the register event for creating custom blocks
          */
         @SubscribeEvent
-        public static void addBlocks(RegistryEvent.Register<Block> event) {
-           /*
+        fun addBlocks(event: RegistryEvent.Register<Block>) {
+            /*
              event.getRegistry().register(new MySpecialBlock().setRegistryName(MOD_ID, "mySpecialBlock"));
             */
         }
+    }
+
+    companion object {
+
+        val MOD_ID = "minecraft-forge-kotlin-template"
+        val MOD_NAME = "Minecraft Forge Kotlin Template"
+        val VERSION = "2019.1-1.2.23"
+
+        /**
+         * This is the instance of your mod as created by Forge. It will never be null.
+         */
+        @Mod.Instance(MOD_ID)
+        var INSTANCE: MinecraftForgeKotlinTemplate? = null
     }
     /* EXAMPLE ITEM AND BLOCK - you probably want these in separate files
     public static class MySpecialItem extends Item {
